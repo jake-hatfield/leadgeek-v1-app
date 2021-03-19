54a1bdda27dc1d8318d9dfbc4916cf54c9f4f020
Jacob Hatfield <jacobmhatfield@gmail.com>
2021-03-19 16:34:09 -0500

    quick view in progress

--- client/src/components/leads/LeadRow.jsx
+++ client/src/components/leads/LeadRow.jsx
@@ -9,6 +9,10 @@ import {
 } from '../../redux/actions/leads';
 // utils
 import { truncate, numberWithCommas, calculateBSR } from '../../utils/utils';
+
+const buttonClasses =
+	'py-1 px-3 w-full text-left font-semibold text-purple-600 hover:bg-gray-100 hover:text-gray-800 transition-colors duration-100 ease-in-out focus:outline-none focus:shadow-outline';
+
 const LeadRow = ({
 	lead,
 	viewLead,
@@ -19,9 +23,10 @@ const LeadRow = ({
 	user,
 }) => {
 	const { data } = lead;
-	// event handlers
 	const [like, setLike] = useState(false);
 	const [newLead, setNewLead] = useState(false);
+	const [quickView, setQuickView] = useState(false);
+	const [expandedView, setExpandedView] = useState(false);
 	useEffect(() => {
 		// set unviewed status
 		if (user.unviewedLeads.some((l) => l._id === lead._id)) {
@@ -43,7 +48,7 @@ const LeadRow = ({
 	};
 	return (
 		<tr
-			className='border-b border-gray-200 hover:bg-gray-100 transition-all duration-100 cursor-pointer'
+			className='relative px-1 border-b border-gray-200 hover:bg-gray-100 transition-all duration-100 cursor-pointer'
 			onClick={() => {
 				newLead && setNewLead(false);
 				setShowDetails(!showDetails);
@@ -67,14 +72,14 @@ const LeadRow = ({
 						handleLikeLead(user._id, lead._id);
 						viewLead(user._id, lead._id);
 					}}
-					className='rounded-md focus:outline-none focus:shadow-outline align-middle'
+					className='p-1 rounded-md focus:outline-none focus:shadow-outline align-middle'
 				>
 					<svg
 						xmlns='http://www.w3.org/2000/svg'
 						fill={`${like ? '#5d55fa' : 'none'}`}
 						viewBox='0 0 24 24'
 						stroke={`${like ? '#5d55fa' : 'currentColor'}`}
-						className='h-6 w-6 hover:text-purple-400 transition-colors duration-100 ease-in-out'
+						className='h-5 w-5 hover:text-purple-400 transition-colors duration-100 ease-in-out'
 					>
 						<path
 							strokeLinecap='round'
@@ -85,7 +90,7 @@ const LeadRow = ({
 					</svg>
 				</button>
 			</td>
-			<td className='p-2 font-semibold'>{truncate(data.title, 28)}</td>
+			<td className='p-2 font-semibold'>{truncate(data.title, 40)}</td>
 			<td className='p-2'>{truncate(data.category, 28)}</td>
 			<td className='p-2'>
 				<span>$</span>
@@ -106,23 +111,143 @@ const LeadRow = ({
 			<td className='p-2'>{numberWithCommas(data.monthlySales)}</td>
 			<td className='p-2'>{numberWithCommas(data.monthlySales)}</td>
 			<td className='p-2'>
-				<button
-					onClick={(e) => {
-						e.stopPropagation();
-						openBothLinks(e, data.sourceLink, data.amzLink);
-					}}
-					className='ml-2 rounded-md focus:outline-none focus:shadow-outline'
-				>
-					<svg
-						xmlns='http://www.w3.org/2000/svg'
-						viewBox='0 0 20 20'
-						fill='currentColor'
-						className='h-6 w-6 text-gray-400'
+				<div className='flex items-center justify-center rounded-r-lg focus:outline-none focus:shadow-outline'>
+					{/* horiztonal dots */}
+					<button
+						onMouseEnter={() => setQuickView(true)}
+						onClick={(e) => {
+							e.stopPropagation();
+							setQuickView(expandedView ? false : true);
+							setExpandedView(!expandedView);
+						}}
+						className={`${
+							quickView
+								? 'absolute z-10 p-2 bg-white shadow-sm rounded-r-lg'
+								: 'rounded-lg'
+						} p-1 text-gray-500 hover:text-gray-700 focus:outline-none focus:shadow-outline`}
 					>
-						<path d='M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z' />
-						<path d='M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z' />
-					</svg>
-				</button>
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							viewBox='0 0 20 20'
+							fill='currentColor'
+							className='h-5 w-5'
+						>
+							<path d='M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z' />
+						</svg>
+					</button>
+					{quickView && (
+						<div className='absolute transform -translate-x-14 bg-white rounded-l-lg shadow-sm text-gray-500'>
+							<div className='flex items-center'>
+								{/* eye */}
+								<button
+									onClick={(e) => {
+										e.stopPropagation();
+										setShowDetails(!showDetails);
+										setCurrentLead(lead);
+									}}
+									className='p-2 rounded-l-lg border-r border-gray-200 hover:text-gray-700 transition-all duration-100 ease-in-out focus:outline-none focus:shadow-outline'
+								>
+									<svg
+										xmlns='http://www.w3.org/2000/svg'
+										viewBox='0 0 20 20'
+										fill='currentColor'
+										className='h-5 w-5'
+									>
+										<path d='M10 12a2 2 0 100-4 2 2 0 000 4z' />
+										<path
+											fillRule='evenodd'
+											d='M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z'
+											clipRule='evenodd'
+										/>
+									</svg>
+								</button>
+								{/* link */}
+								<button
+									onClick={(e) => {
+										e.stopPropagation();
+										openBothLinks(e, data.sourceLink, data.amzLink);
+									}}
+									className='p-2 border-r border-gray-200 hover:text-gray-700 transition-all duration-100 ease-in-out focus:outline-none focus:shadow-outline'
+								>
+									<svg
+										xmlns='http://www.w3.org/2000/svg'
+										viewBox='0 0 20 20'
+										fill='currentColor'
+										className='h-5 w-5 hover:text-gray-700'
+									>
+										<path
+											fillRule='evenodd'
+											d='M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z'
+											clipRule='evenodd'
+										/>
+									</svg>
+								</button>
+							</div>
+						</div>
+					)}
+					{expandedView && (
+						<div className='absolute z-20 w-32 transform -translate-x-16 bg-white rounded-lg shadow-md'>
+							<div className='py-2 border-b border-gray-200'>
+								<button
+									onClick={(e) => {
+										e.stopPropagation();
+										newLead && setNewLead(false);
+										setLike(!like);
+										handleLikeLead(user._id, lead._id);
+										viewLead(user._id, lead._id);
+									}}
+									className={buttonClasses}
+								>
+									{!like ? 'Like lead' : 'Unlike lead'}
+								</button>
+								<button
+									onClick={(e) => {
+										e.stopPropagation();
+										newLead && setNewLead(false);
+										setLike(!like);
+										handleLikeLead(user._id, lead._id);
+										viewLead(user._id, lead._id);
+									}}
+									className={buttonClasses}
+								>
+									Archive lead
+								</button>
+							</div>
+							<div className='pt-2 pb-4'>
+								<button className={`${buttonClasses} flex items-center`}>
+									<span>Open links</span>
+									<svg
+										xmlns='http://www.w3.org/2000/svg'
+										viewBox='0 0 20 20'
+										fill='currentColor'
+										className='ml-1 h-4 w-4'
+									>
+										<path
+											fillRule='evenodd'
+											d='M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z'
+											clipRule='evenodd'
+										/>
+									</svg>
+								</button>
+								<button className={`${buttonClasses} flex items-center`}>
+									<span>View details</span>
+									<svg
+										xmlns='http://www.w3.org/2000/svg'
+										viewBox='0 0 20 20'
+										fill='currentColor'
+										className='ml-1 h-4 w-4'
+									>
+										<path
+											fillRule='evenodd'
+											d='M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z'
+											clipRule='evenodd'
+										/>
+									</svg>
+								</button>
+							</div>
+						</div>
+					)}
+				</div>
 			</td>
 		</tr>
 	);
