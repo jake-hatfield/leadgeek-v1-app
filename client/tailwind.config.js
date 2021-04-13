bd309309006f0cd34363393766404a02eef60f59
Jacob Hatfield <jacobmhatfield@gmail.com>
2021-04-13 16:19:59 -0500

    purge tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
 	future: {
 		removeDeprecatedGapUtilities: true,
 	},
-	purge: false,
+	purge: ['./src/**/*.html', './src/**/*.jsx', './src/**/*.js'],
 	theme: {
 		extend: {
 			colors: {
