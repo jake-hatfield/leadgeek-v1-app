d649273b9381b13053785ca28cc565589f86661c
Jacob Hatfield <jacobmhatfield@gmail.com>
2021-04-16 11:48:15 -0500

    env changes

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.use(express.json({ extended: false }));
 // define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/leads', require('./routes/api/leads'));
 app.use('/api/search', require('./routes/api/search'));
