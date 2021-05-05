d5d2f23ecaf0473ea5d8046d505f6ca667400ff2
Jacob Hatfield <jacobmhatfield@gmail.com>
2021-05-05 14:42:55 -0500

    pagination style fix

--- client/src/components/layout/navigation/Pagination.jsx
+++ client/src/components/layout/navigation/Pagination.jsx
@@ -40,7 +40,7 @@ const Pagination = ({
 					!noPadding && 'container'
 				} text-gray-600`}
 			>
-				{filteredItems > 0 && (
+				{filteredItems > 0 ? (
 					<div className='flex items-center text-sm'>
 						<span>View</span>
 						<select
@@ -63,48 +63,53 @@ const Pagination = ({
 						</select>
 						<span>results per page</span>
 					</div>
+				) : (
+					<div />
 				)}
-				<div className='flex items-center text-sm'>
-					<div className='relative flex items-center'>
-						{filteredItems > 0 && totalItems !== filteredItems && (
-							<button
-								onClick={() => setFilteredMessage((prev) => !prev)}
-								onMouseEnter={() => setFilteredMessage(true)}
-								onMouseLeave={() => setFilteredMessage(false)}
-								className='mr-2 rounded-md text-gray-500 hover:text-gray-700 transition duration-100 ease-in-out ring-gray'
-							>
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									className='h-5 w-5'
-									viewBox='0 0 20 20'
-									fill='currentColor'
+				<div className='flex items-center justify-between text-sm'>
+					{filteredItems ? (
+						<div className='relative flex items-center'>
+							{filteredItems > 0 && totalItems > filteredItems && (
+								<button
+									onClick={() => setFilteredMessage((prev) => !prev)}
+									onMouseEnter={() => setFilteredMessage(true)}
+									onMouseLeave={() => setFilteredMessage(false)}
+									className='mr-2 rounded-md text-gray-500 hover:text-gray-700 transition duration-100 ease-in-out ring-gray'
 								>
-									<path
-										fillRule='evenodd'
-										d='M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z'
-										clipRule='evenodd'
-									/>
-								</svg>
-								{filteredMessage && (
-									<div className='absolute z-10 bottom-0 py-2 px-4 transform -translate-y-8 rounded-md shadow-md bg-gray-800 text-left text-white text-sm'>
-										<p>
-											<span className='font-semibold'>
-												{totalItems - filteredItems}
-											</span>{' '}
-											leads aren't showing because of applied filters.
-										</p>
-									</div>
-								)}
-							</button>
-						)}
-						{filteredItems ? (
-							<span>
-								Showing {itemsFrom} to {itemsTo} of {filteredItems} results
-							</span>
-						) : (
-							<div />
-						)}
-					</div>
+									<svg
+										xmlns='http://www.w3.org/2000/svg'
+										className='h-5 w-5'
+										viewBox='0 0 20 20'
+										fill='currentColor'
+									>
+										<path
+											fillRule='evenodd'
+											d='M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z'
+											clipRule='evenodd'
+										/>
+									</svg>
+									{filteredMessage && (
+										<div className='absolute z-10 bottom-0 py-2 px-4 transform -translate-y-8 rounded-md shadow-md bg-gray-800 text-left text-white text-sm'>
+											<p>
+												<span className='font-semibold text-teal-400'>
+													{totalItems - filteredItems}
+												</span>{' '}
+												leads aren't showing because of applied filters.
+											</p>
+										</div>
+									)}
+								</button>
+							)}
+							{filteredItems && (
+								<span>
+									Showing {itemsFrom} to {itemsTo} of {filteredItems} results
+								</span>
+							)}
+						</div>
+					) : (
+						<div />
+					)}
+
 					{(hasPreviousPage || hasNextPage) && (
 						<div>
 							<Link
