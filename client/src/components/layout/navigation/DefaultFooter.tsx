3bd1cb53b1a5465f735b6d7b26b94785f9100035
Jacob Hatfield <jacobmhatfield@gmail.com>
2021-10-02 22:51:55 -0500

    updated favicon

--- client/src/components/layout/navigation/DefaultFooter.tsx
+++ client/src/components/layout/navigation/DefaultFooter.tsx
@@ -4,8 +4,7 @@ const DefaultFooter = () => {
 	return (
 		<footer className='mt-6 xl:mt-0 mb-6 container'>
 			<span className='bg-white'>
-				&copy; 2020 - {new Date().getFullYear()} LeadGeek, Inc. All rights
-				reserved.
+				&copy; 2020 - {new Date().getFullYear()} Leadgeek. All rights reserved.
 			</span>
 		</footer>
 	);
