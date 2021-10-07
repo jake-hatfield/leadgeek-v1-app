5aa9e7ef260c8f22c6a94967e85a8ce1d1ebbfb0
Jacob Hatfield <jacobmhatfield@gmail.com>
2021-10-07 16:17:52 -0500

    user table and admin tools, componentized toggle

--- src/routes/api/users.ts
+++ src/routes/api/users.ts
@@ -157,7 +157,7 @@ router.post(
 // @route       POST api/clear-notification
 // @description Clear a user's notification by ID
 // @access      Private
-router.post(
+router.put(
 	'/clear-notification',
 	auth,
 	async (
@@ -197,219 +197,47 @@ router.post(
 	}
 );
 
-// @route       POST api/users
-// @description Register user
-// @access      Public
-// router.post('/', async (req, res) => {
-// 	// destructure name, email and pw out of the body
-// 	const { name, email, password } = req.body;
-// 	if (!name || !email || !password) {
-// 		return res.status(400).json({
-// 			errors: [
-// 				{ message: 'Please make sure all required information is present' },
-// 			],
-// 		});
-// 	}
-// 	try {
-// 		// see if user exists in stripe
-// 		const stripeUser = await stripe.customers.list({ email });
-// 		if (stripeUser.data.length === 0) {
-// 			return res.status(400).json({
-// 				errors: [
-// 					{
-// 						message:
-// 							"There's no LeadGeek subscription associated with that email. Please try another email or sign up for a plan.",
-// 					},
-// 				],
-// 			});
-// 		}
-// 		// see if user exists in db
-// 		let user = await User.findOne({ email });
-// 		if (user) {
-// 			return res.status(400).json({
-// 				errors: [
-// 					{
-// 						message:
-// 							'An account already exists under that email. Please try a different email or log in.',
-// 					},
-// 				],
-// 			});
-// 		}
-// 		const userData = stripeUser.data[0];
-// 		// retrieve the payment method details
-// 		const userPM = await stripe.paymentMethods.list({
-// 			customer: userData.id,
-// 			type: 'card',
-// 		});
-// 		user = new User({
-// 			name,
-// 			email,
-// 			password,
-// 			dateCreated: userData.created,
-// 			lastLogin: Date.now(),
-// 			subscription: {
-// 				cusId: userData.id,
-// 				subId: {
-// 					id: userData.subscriptions.data[0].id,
-// 					active: userData.subscriptions.data[0].active,
-// 				},
-// 				planId: userData.subscriptions.data[0].plan.id,
-// 			},
-// 			billing: {
-// 				paymentMethod: userData.invoice_settings.default_payment_method,
-// 				last4: userPM.data[0].card.last4,
-// 				brand: userPM.data[0].card.brand,
-// 			},
-// 		});
-// 		// encrypt password
-// 		const salt = await bcrypt.genSalt(10);
-// 		user.password = await bcrypt.hash(password, salt);
-// 		await user.save();
-// 		// return the JWT
-// 		const payload = {
-// 			user: {
-// 				id: user.id,
-// 			},
-// 		};
+router.get('/all/:id', auth, async (req: Request, res: Response) => {
+	try {
+		// destructure necessary items
+		const { id } = req.params;
 
-// 		jwt.sign(payload, jwtSecret, { expiresIn: 60 * 120 }, (err, token) => {
-// 			if (err) throw err;
-// 			res.json({ token });
-// 		});
-// 	} catch (err) {
-// 		console.error(err.message);
-// 		// if there's an error, it's got to be with the server
-// 		return res.status(500).json({
-// 			errors: [
-// 				{
-// 					message:
-// 						'There was an error creating your account. Please try again later or contact support.',
-// 				},
-// 			],
-// 		});
-// 	}
-// });
-
-router.post(
-	'/get-all-users',
-	auth,
-	async (
-		req: Request<{}, {}, { page: number; itemLimit: number }>,
-		res: Response
-	) => {
-		try {
-			// destructure necessary items
-			const { page, itemLimit } = req.body;
-
-			let totalItems;
-
-			// lookup all users
-			const users = await User.find({})
-				.countDocuments()
-				.then((numUsers) => {
-					totalItems = numUsers;
-					return User.find({})
-						.skip((page - 1) * (itemLimit || ITEMS_PER_PAGE))
-						.limit(itemLimit || ITEMS_PER_PAGE)
-						.sort({ dateCreated: -1 });
-				});
-
-			// there are users found
-			if (users.length > 0) {
-				return res.status(200).send({
-					users,
-					page,
-					hasNextPage:
-						(itemLimit || ITEMS_PER_PAGE) * page <
-						((totalItems && totalItems) || 1),
-					hasPreviousPage: page > 1,
-					nextPage: page + 1,
-					previousPage: page - 1,
-					lastPage:
-						totalItems && Math.ceil(totalItems / (itemLimit || ITEMS_PER_PAGE)),
-					totalItems,
-				});
-			} else {
-				return res.status(200).send({
-					users: [],
-					page: 1,
-					hasNextPage: false,
-					hasPreviousPage: false,
-					nextPage: 2,
-					previousPage: 0,
-					lastPage: null,
-					totalItems: 0,
-					message:
-						'There was an error fetching all users. You done something wrong, boy.',
-				});
-			}
-		} catch (error) {
-			console.log(error);
+		if (!req.params.id) {
+			return res.status(401).send({
+				users: [],
+				message: 'Access prohibited',
+			});
 		}
+
+		const master = await User.find({ _id: id, role: 'master' });
+
+		if (!master) {
+			return res.status(401).send({
+				users: [],
+				message: 'Access prohibited',
+			});
+		}
+
+		// lookup all users
+		const users = await User.find({}).sort({ dateCreated: -1 });
+
+		// there are users found
+		if (users.length > 0) {
+			return res.status(200).send({
+				users,
+				message: 'Returning all users',
+			});
+		} else {
+			return res.status(200).send({
+				users: [],
+				message:
+					'There was an error fetching all users. You done something wrong, boy',
+			});
+		}
+	} catch (error) {
+		console.log(error);
 	}
-);
-
-// @route       POST api/cancel-subscription
-// @description Cancel current user's requested stripe subscription
-// @access      Private
-// router.post('/cancel-subscription', async (req, res) => {
-// 	try {
-// 		const { subscriptionId } = req.body;
-// 		const retrievedSub = await stripe.subscriptions.retrieve(subscriptionId);
-// 		console.log(retrievedSub);
-// 		if (!retrievedSub) {
-// 			return res
-// 				.status(200)
-// 				.json({ message: 'Subscription could not be canceled.' });
-// 		}
-// 		if (retrievedSub.status === 'canceled') {
-// 			return res
-// 				.status(200)
-// 				.json({ message: 'This subscription is already canceled.' });
-// 		} else {
-// 			const canceledSubscription = await stripe.subscriptions.del(
-// 				subscriptionId
-// 			);
-// 			if (canceledSubscription.status === 'canceled') {
-// 				return res.json({
-// 					message: 'Subscription was successfully canceled.',
-// 					subscription: canceledSubscription,
-// 				});
-// 			} else {
-// 				return res.json({
-// 					message: 'There was an error trying to cancel this subscription.',
-// 				});
-// 			}
-// 		}
-// 	} catch (error) {
-// 		console.error(error.message);
-// 		res.status(500).send('Server error');
-// 	}
-// });
-
-// router.post('/update-db-subscription', async (req, res) => {
-// 	try {
-// 		const { customerId, subscription } = req.body;
-// 		const { id } = subscription;
-// 		let user = await User.findOne({ customerId });
-// 		let subArray = user.subId;
-// 		const index = subArray.findIndex((sub) => sub.id === id);
-// 		if (index !== -1) {
-// 			subArray[index] = subscription;
-// 			user.update({ subId: subArray }, function (error) {
-// 				if (error) console.log(error);
-// 			});
-// 		} else {
-// 			console.log('Subscription index not found.');
-// 			return res.status(200).json({
-// 				message: 'There was an error in updating your subscription.',
-// 			});
-// 		}
-// 	} catch (error) {
-// 		console.error(error.message);
-// 		res.status(500).send('Server error');
-// 	}
-// });
+});
 
 // @route       POST api/notifications
 // @description Retrieve a user's notifications
@@ -502,29 +330,5 @@ router.post(
 		}
 	}
 );
-
-// router.put('/update-affiliate-paypal', auth, async (req, res) => {
-// 	try {
-// 		const { id, newEmail: email } = req.body;
-// 		const affiliate = await User.findOneAndUpdate(
-// 			{ _id: id },
-// 			{ 'referrals.referrer.paypalEmail': email }
-// 		);
-// 		if (affiliate) {
-// 			return res.status(200).json({
-// 				status: 'success',
-// 				message: 'Your PayPal email was updated',
-// 			});
-// 		} else {
-// 			return res.status(200).json({
-// 				status: 'failure',
-// 				message: 'Your PayPal email could not be updated',
-// 			});
-// 		}
-// 	} catch (error) {
-// 		console.log(error);
-// 		res.status(500).send('Server error');
-// 	}
-// });
 
 module.exports = router;
